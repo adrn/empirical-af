158606b64f96c758dab7ec735d040d312f87c641
Adrian Price-Whelan <adrian.prw@gmail.com>
2023-07-21 15:31:28 -0400

    moving important notebooks into tex

---
+++ tex/figures/helpers.py
@@ -0,0 +1,26 @@
+import agama
+import astropy.units as u
+import gala.potential as gp
+from gala.units import galactic
+
+agama.setUnits(mass=u.Msun, length=u.kpc, time=u.Myr)
+
+
+gala_pot = gp.CCompositePotential(
+    disk=gp.MiyamotoNagaiPotential(m=6.91e10, a=3, b=0.25, units=galactic),
+    halo=gp.NFWPotential(m=5.4e11, r_s=15.0, units=galactic),
+)
+
+agama_pot = agama.Potential(
+    dict(
+        type="miyamotonagai",
+        mass=gala_pot["disk"].parameters["m"].value,
+        scaleradius=gala_pot["disk"].parameters["a"].value,
+        scaleheight=gala_pot["disk"].parameters["b"].value,
+    ),
+    dict(
+        type="nfw",
+        mass=gala_pot["halo"].parameters["m"].value,
+        scaleradius=gala_pot["halo"].parameters["r_s"].value,
+    ),
+)
